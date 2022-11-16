9c8a5b6e3c9c658179f0f16bfe987bbec468a1de
marcoluchi11 <marcoaluchi@hotmail.com>
2022-11-16 17:02:48 -0300

    add points feature

--- src/components/Auth/LoginMenu.jsx
+++ src/components/Auth/LoginMenu.jsx
@@ -3,6 +3,8 @@ import Profile from "./Profile";
 import Login from "./Login";
 import Logout from "./Logout";
 import styled from "styled-components";
+import { useContext } from "react";
+import { PokeContext } from "../../context/PokeContext";
 const Container = styled.div`
   @media (min-width: 720px) {
     display: flex;
@@ -11,10 +13,12 @@ const Container = styled.div`
 `;
 const LoginMenu = () => {
   const { isAuthenticated } = useAuth0();
+  const { points } = useContext(PokeContext);
   return isAuthenticated ? (
     <Container>
       <Profile />
       <Logout />
+      <h1>Points: {points}</h1>
     </Container>
   ) : (
     <Container>
