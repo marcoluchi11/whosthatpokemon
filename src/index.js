c9f7200047f68c86433ab57d963833698ae43bd7
marcoluchi11 <marcoaluchi@hotmail.com>
2022-11-16 17:00:37 -0300

    create and set context

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
+import PokeProvider from "./context/PokeContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Auth0Provider
-      domain="dev-gnw5tr-c.us.auth0.com"
-      clientId="gOJMcSwhKCyL8bBJROtAzui2wq3guXpa"
-      redirectUri={window.location.origin}
-    >
-      <App />
-    </Auth0Provider>
+    <PokeProvider>
+      <Auth0Provider
+        domain="dev-gnw5tr-c.us.auth0.com"
+        clientId="gOJMcSwhKCyL8bBJROtAzui2wq3guXpa"
+        redirectUri={window.location.origin}
+      >
+        <App />
+      </Auth0Provider>
+    </PokeProvider>
   </React.StrictMode>
 );
